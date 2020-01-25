239a04589a7ffb3bfa76b315a18b16d87b3b0548
Abhimanyu <desk.abhimanyu@gmail.com>
2020-01-26 02:38:27 +0530

    Added install_daemon_dir.sh

---
+++ daemon/functions/install_daemon_dir.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+local command_name="INSTALL"
+local command_action="Install"
+local function_self_name=$(basename "$(readlink -f "${BASH_SOURCE[0]}")")
+
+echo -e ""
+echo -e "${lightyellow}Daemon Directory${default}"
+echo -e "================================="
+fn_sleep_time
+if [ -d "${daemon_files}" ]; then
+  fn_print_warning_nl "A daemon is already installed here"
+fi
+pwd
+echo -e ""
+if ! fn_prompt_yn "Continue?" Y; then
+  exit
+fi
+
+if [ ! -d "${daemon_files}" ]; then
+  mkdir -v "${daemon_files}"
+fi

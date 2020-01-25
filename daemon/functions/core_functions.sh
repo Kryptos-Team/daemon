9e58dcc94b78ae2dbe17655e9c20446001a2a21a
Abhimanyu <desk.abhimanyu@gmail.com>
2020-01-26 01:30:15 +0530

    Updated core_functions.sh

--- daemon/functions/core_functions.sh
+++ daemon/functions/core_functions.sh
@@ -10,9 +10,156 @@ core_messages.sh() {
   fn_bootstrap_fetch_file_github "daemon/functions" "core_messages.sh" "${functions_dir}" "chmodx" "run" "noforcedl" "nomd5"
 }
 
+core_exit.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
 core_trap.sh() {
   function_file="${FUNCNAME[0]}"
-  fn_bootstrap_fetch_file_github "daemon/functions" "core_trap.sh" "${functions_dir}" "chmodx" "run" "noforcedl" "nomd5"
+  fn_fetch_function
+}
+
+# Commands
+command_debug.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+command_details.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+command_start.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+command_stop.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+command_install.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+command_restart.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+command_wipe.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+# Checks
+check.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+check_deps.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+check_executable.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+check_ip.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+check_logs.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+check_permissions.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+check_root.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+check_tmuxexception.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+# Info
+info_status.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+info_distro.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+# Logs
+
+logs.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+# Update
+command_update_functions.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+command_update_daemon.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+command_update.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+fn_update_functions.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+# Installer functions
+install_complete.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+install_logs.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+install_retry.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+install_daemon_dir.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
+}
+
+install_daemon_files.sh() {
+  function_file="${FUNCNAME[0]}"
+  fn_fetch_function
 }
 
 # Creates tmp dir if missing

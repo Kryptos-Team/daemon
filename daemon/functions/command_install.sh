d17fd1a78e52c2d15ce4095fc065e573a39df166
Abhimanyu <desk.abhimanyu@gmail.com>
2020-01-26 23:52:10 +0530

    Added command_install.sh

---
+++ daemon/functions/command_install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+local command_name="INSTALL"
+local command_action="Install"
+local function_self_name=$(basename "$(readlink -f "${BASH_SOURCE[0]}")")
+
+check.sh
+
+if [ "$(whoami)" == "root" ]; then
+  check_deps.sh
+else
+  install_header.sh
+  install_daemon_dir.sh
+  install_logs.sh
+  check_deps.sh
+  install_daemon_files.sh
+  install_config.sh
+  install_daemon.sh
+  install_complete.sh
+fi
+
+core_exit.sh

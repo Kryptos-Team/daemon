5d27430179e15cca8c1762260a7bf8d32b946c07
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2020-02-03 05:33:25 -0800

    Finished update daemons logic

--- daemon/functions/command_update_daemon.sh
+++ daemon/functions/command_update_daemon.sh
@@ -20,11 +20,32 @@ if [ -n "${functions_dir}" ]; then
       exitcode=$?
       function_file_diff=$(diff "${functions_dir}/${function_file}" <(curl --fail -s "https://raw.githubusercontent.com/${githubuser}/${githubrepo}/${githubbranch}/${github_file_url_dir}/${function_file}"))
       if [ "${exitcode}" -ne 0 ]; then
-        fn_print_fail_eol_nl
-        echo -en "removing unknown function ${function_file}\n"
+        fn_print_fail_eol_nl "removing unknown function ${function_file}\n"
         fn_script_log_fatal "removing unknown function ${function_file}"
-
+        if ! rm -f "${function_file}"; then
+          fn_print_fail_eol_nl
+          core_exit.sh
+        else
+          fn_print_ok_eol_nl
         fi
+      elif [ "${function_file_diff}" != "" ]; then
+        fn_print_update_eol_nl
+        fn_script_log_info "checking function ${function_file}: UPDATE"
+        rm -rf "${functions_dir:?}/${function_file}"
+        fn_update_function
+      else
+        fn_print_ok_eol_nl
+      fi
     done
   fi
 fi
+
+if [ "${exitcode}" != "0" ] && [ -n "${exitcode}" ]; then
+  fn_print_fail "Updating functions"
+  fn_script_log_fatal "Updating functions"
+else
+  fn_print_ok "Updating functions"
+  fn_script_log_pass "Updating functions"
+fi
+
+core_exit.sh

dc5a9382be9e54b727465c337f345800f771c84c
Abhimanyu <desk.abhimanyu@gmail.com>
2020-02-08 19:50:33 +0530

    Added .installed after successful installation

--- daemon/functions/install_complete.sh
+++ daemon/functions/install_complete.sh
@@ -6,6 +6,9 @@ local function_self_name=$(basename "$(readlink -f "${BASH_SOURCE[0]}")")
 
 echo -e ""
 fn_print_dash
+
+touch "${daemon_installed}"
+
 echo -e "Install Complete!"
 fn_script_log_info "Install Complete!"
 echo -e ""
